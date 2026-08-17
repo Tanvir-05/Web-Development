465c1c4b932df4cad4b1ab7d8134cfb655f8bd71
Ur_Laazie_boi <tanvir1234567890bd@gmail.com>
2026-08-17 22:29:49 +0600

    Add platform and appVersion to electronAPI
    Expose platform and appVersion to renderer process.

--- preload.js
+++ preload.js
@@ -2,6 +2,8 @@ const { contextBridge } = require('electron');
 
 // Expose minimal safe APIs to the renderer process
 contextBridge.exposeInMainWorld('electronAPI', {
+  
   platform: process.platform,
+  
   appVersion: '1.0.0'
 });
